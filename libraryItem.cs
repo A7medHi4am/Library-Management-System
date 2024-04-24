4f68420ee74cafcc52e84cf5a5119bb2a0c8513b
A7medHi4am <107587146+A7medHi4am@users.noreply.github.com>
2024-04-24 21:10:42 +0200

    Add files via upload

--- libraryItem.cs
+++ libraryItem.cs
@@ -12,4 +12,34 @@ public abstract class LibraryItem
     public abstract void CheckOut();
     public abstract void CheckIn();
 }
+public class Book : LibraryItem
+{
+    public string ISBN { get; set; }
+    public string Genre { get; set; }
+
+    public override void CheckOut()
+    {
+
+    }
+
+    public override void CheckIn()
+    {
+
+    }
+}
+public class DVD : LibraryItem
+{
+    public string Director { get; set; }
+    public int Runtime { get; set; }
+
+    public override void CheckOut()
+    {
+
+    }
+
+    public override void CheckIn()
+    {
+
+    }
+}
 
